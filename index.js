506e561e026c580a8d0f17f313e901e8fb629836
karen chehade <karen_chehade@hotmail.com>
2021-05-03 18:10:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-function submitData( name, email ) {
-  const option ={
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify( {
-        name,
-        email
-      } )
-    } ;
-  return fetch( 'http://localhost:3000/users', option )
-    .then( function ( response ) {
-      return response.json()
-    } )
-    .then( function ( object ) {
-      document.body.innerHTML = object[ "id" ]
-    } )
-    .catch( function ( error ) {
-      document.body.innerHTML = error.message
-    } )
+function submitData(name, email){
+  const option = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name,email})
+  }
+  return fetch("http://localhost:3000/users", option)
+  .then(response => response.json())
+  .then(function(object){
+     document.body.innerHTML = object[ "id" ];
+  })
+
 }
+
